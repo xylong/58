53c19b8f70d31ee55c59b7523288362c3367757c
xyl <416319808@qq.com>
2019-06-11 23:20:17 +0800

    dingo结合jwt

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use Dingo\Api\Routing\Helpers;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    use Helpers;
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth('api')->attempt($credentials)) {
+//            return response()->json(['error' => 'Unauthorized'], 401);
+            return $this->response->errorUnauthorized();
+        }
+
+//        return $this->respondWithToken($token);
+        return $this->response->array(['token' => $token]);
+    }
+
+    public function me()
+    {
+//        return response()->json(auth('api')->user());
+        return auth('api')->user();
+    }
+
+    public function logout()
+    {
+        auth('api')->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth('api')->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,25 +11,40 @@ use Illuminate\Http\Request;
 |
 */
 
+$api = app('Dingo\Api\Routing\Router');
 
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api\V1',
+    'middleware' => ['bindings']
+], function ($api) {
 
-Route::group([
-    'prefix' => 'auth'
-], function ($router) {
-    Route::post('login', 'AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
+    $api->group([
+        'prefix' => 'auth',
+    ], function ($api) {
+        $api->post('login', 'AuthController@login');
+        $api->post('logout', 'AuthController@logout');
+        $api->post('refresh', 'AuthController@refresh');
+        $api->post('me', 'AuthController@me')->middleware('auth:api');
+    });
 });
 
-Route::group([
-    'prefix' => 'admin'
-], function ($router) {
-    Route::post('login', 'AuthAdminController@login');
-    Route::post('logout', 'AuthAdminController@logout');
-    Route::post('refresh', 'AuthAdminController@refresh');
-    Route::post('me', 'AuthAdminController@me');
-});
-
-Route::middleware('auth:api')->get('/apple', 'TestController@apple');
-Route::middleware('auth:admin')->get('/banana', 'TestController@banana');
+//Route::group([
+//    'prefix' => 'auth'
+//], function ($router) {
+//    Route::post('login', 'AuthController@login');
+//    Route::post('logout', 'AuthController@logout');
+//    Route::post('refresh', 'AuthController@refresh');
+//    Route::post('me', 'AuthController@me');
+//});
+//
+//Route::group([
+//    'prefix' => 'admin'
+//], function ($router) {
+//    Route::post('login', 'AuthAdminController@login');
+//    Route::post('logout', 'AuthAdminController@logout');
+//    Route::post('refresh', 'AuthAdminController@refresh');
+//    Route::post('me', 'AuthAdminController@me');
+//});
+//
+//Route::middleware('auth:api')->get('/apple', 'TestController@apple');
+//Route::middleware('auth:admin')->get('/banana', 'TestController@banana');
